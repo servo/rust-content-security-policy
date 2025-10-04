22b2831433f306ad22420dca4f5ff685b7fe4cc5
Tim van der Lippe <tvanderlippe@gmail.com>
2025-10-04 20:27:45 +0200

    Make destination hashable
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -784,7 +784,7 @@ pub enum Initiator {
 }
 
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum Destination {
     None,
     Audio,
